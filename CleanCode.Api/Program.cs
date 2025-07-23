83ef4369031de88497c05850e008e5bb8070a854
andresrios72 <andresrios72@gmail.com>
2025-07-23 01:56:39 -0500

    test: Adicion de titulo en swagger para verificar CI/CD

--- CleanCode.Api/Program.cs
+++ CleanCode.Api/Program.cs
@@ -96,7 +96,10 @@ builder.Services.AddSwaggerGen(setup =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanCode API", Version = "v1" });
+});
 
 var app = builder.Build();
 
