c73d59c2ec20eb4143a55b1cd153864c3bacdcdf
andresrios72 <andresrios72@gmail.com>
2025-07-23 02:31:41 -0500

    Update Titulo
    Cambio titulo swagger

--- CleanCode.Api/Program.cs
+++ CleanCode.Api/Program.cs
@@ -98,7 +98,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanCode API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanCode AWS API", Version = "v1" });
 });
 
 var app = builder.Build();
